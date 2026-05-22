b66758529c6bf5771a1c01e4dff92372cb51ac20
James Metz <jamesmetz@onshelfmarketing.com>
2026-05-22 12:25:41 -0400

    adding colors to local cli game and adding github link to html

--- run/run.go
+++ run/run.go
@@ -5,16 +5,71 @@ import (
 	"distasteful-bear/turing_machine/verifiers"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+const (
+	ansiReset  = "\033[0m"
+	ansiBold   = "\033[1m"
+	ansiBlue   = "\033[34m"
+	ansiYellow = "\033[33m"
+	ansiPurple = "\033[35m"
+	ansiGreen  = "\033[32m"
+	ansiRed    = "\033[31m"
+)
+
+func colorize(text, color string) string {
+	return color + text + ansiReset
+}
+
+func colorName(name string) string {
+	switch name {
+	case "Blue", "blue":
+		return colorize(name, ansiBlue)
+	case "Yellow", "yellow":
+		return colorize(name, ansiYellow)
+	case "Purple", "purple":
+		return colorize(name, ansiPurple)
+	default:
+		return name
+	}
+}
+
+func colorizeVerifierDesc(desc string) string {
+	desc = strings.ReplaceAll(desc, "blue", colorName("blue"))
+	desc = strings.ReplaceAll(desc, "yellow", colorName("yellow"))
+	desc = strings.ReplaceAll(desc, "purple", colorName("purple"))
+	return desc
+}
+
+func formatSolution(sol verifiers.Solution) string {
+	return fmt.Sprintf("%s%s%s",
+		colorize(string(sol.Display[0]), ansiBlue),
+		colorize(string(sol.Display[1]), ansiYellow),
+		colorize(string(sol.Display[2]), ansiPurple),
+	)
+}
+
+func printColorLegend() {
+	fmt.Println("Digit colors / positions:")
+	fmt.Printf("  %s digit = 1st digit in your guess\n", colorName("Blue"))
+	fmt.Printf("  %s digit = 2nd digit in your guess\n", colorName("Yellow"))
+	fmt.Printf("  %s digit = 3rd digit in your guess\n", colorName("Purple"))
+	fmt.Println("Example: 421 means Blue=4, Yellow=2, Purple=1.")
+}
 
 func printVerifiers(displayOnly bool, vs [4]verifiers.VerifierBranch, guess verifiers.Solution) {
 	for i, v := range vs {
-		fmt.Printf("Verifier %v: ", (i + 1))
-		fmt.Println(v.Desc)
+		fmt.Printf("%sVerifier %v:%s ", ansiBold, (i + 1), ansiReset)
+		fmt.Println(colorizeVerifierDesc(v.Desc))
 		if !displayOnly {
 			result := v.VerifierFunc(guess)
 			fmt.Print("Result: ")
-			fmt.Println(result)
+			if result {
+				fmt.Println(colorize("true", ansiGreen))
+			} else {
+				fmt.Println(colorize("false", ansiRed))
+			}
 		}
 	}
 }
@@ -43,12 +98,14 @@ func enterGuess() verifiers.Solution {
 
 func RunLocalSinglePlayer(p verifiers.Puzzle) {
 	utils.CallClear()
-	fmt.Print("Welcome to the Turing Machine Game.\n\n")
+	fmt.Printf("%sWelcome to the Turing Machine Game.%s\n\n", ansiBold, ansiReset)
 
-	fmt.Println("A Solution has been generated. \nBelow are your verififers.")
+	fmt.Println("A Solution has been generated. Below are your verifiers.")
 	fmt.Println("When you enter a guess, each verifier will return whether the test was successful.")
 	fmt.Println("Use this information to deduce the solution.")
 	fmt.Print("When you feel confident, you can end the guessing and check your work.\n\n")
+	printColorLegend()
+	fmt.Print("\n")
 
 	checkAnswer := false
 
@@ -72,11 +129,13 @@ func RunLocalSinglePlayer(p verifiers.Puzzle) {
 
 		// display results
 		utils.CallClear()
-		fmt.Printf("Guess: %c\n", sol.Display)
+		printColorLegend()
 		fmt.Print("\n")
-		fmt.Printf("Blue: %c\n", sol.Display[0])
-		fmt.Printf("Yellow: %c\n", sol.Display[1])
-		fmt.Printf("Purple: %c\n", sol.Display[2])
+		fmt.Printf("Guess: %s\n", formatSolution(sol))
+		fmt.Print("\n")
+		fmt.Printf("%s: %c\n", colorName("Blue"), sol.Display[0])
+		fmt.Printf("%s: %c\n", colorName("Yellow"), sol.Display[1])
+		fmt.Printf("%s: %c\n", colorName("Purple"), sol.Display[2])
 		fmt.Print("\n\n")
 		printVerifiers(false, p.Vers, sol)
 
@@ -103,11 +162,11 @@ func RunLocalSinglePlayer(p verifiers.Puzzle) {
 		} else {
 			fmt.Println("Failure! You have not solved the puzzle.")
 			fmt.Println("The answer was:")
-			fmt.Printf("Guess: %c\n", sol.Display)
+			fmt.Printf("Guess: %s\n", formatSolution(p.Sol))
 			fmt.Print("\n")
-			fmt.Printf("Blue: %c\n", sol.Display[0])
-			fmt.Printf("Yellow: %c\n", sol.Display[1])
-			fmt.Printf("Purple: %c\n", sol.Display[2])
+			fmt.Printf("%s: %c\n", colorName("Blue"), p.Sol.Display[0])
+			fmt.Printf("%s: %c\n", colorName("Yellow"), p.Sol.Display[1])
+			fmt.Printf("%s: %c\n", colorName("Purple"), p.Sol.Display[2])
 			fmt.Print("\n\n")
 			return
 		}
